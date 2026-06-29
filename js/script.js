7b2905036b6e9c5d6d837642528acb6939176db0
Disha Tawar <disha26tawar@gmail.com>
2026-06-29 17:16:06 +0530

    Create NGO registration form with validation

--- js/script.js
+++ js/script.js
@@ -14,3 +14,39 @@ ngoForm.reset();
 });
 
 }
+
+// NGO Form
+
+const ngoForm = document.getElementById("ngoForm");
+
+if (ngoForm) {
+
+ngoForm.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("NGO Registered Successfully!");
+
+ngoForm.reset();
+
+});
+
+}
+
+// Volunteer Form
+
+const volunteerForm = document.getElementById("volunteerForm");
+
+if (volunteerForm){
+
+volunteerForm.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Volunteer Registered Successfully!");
+
+volunteerForm.reset();
+
+});
+
+}
