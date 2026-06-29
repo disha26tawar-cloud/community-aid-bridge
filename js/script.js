b37bb6c0e87bc106d8b769759f256e5419ea1193
Disha Tawar <disha26tawar@gmail.com>
2026-06-29 16:46:22 +0530

    Design responsive homepage with hero section and feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,68 @@
 console.log("Community Aid Bridge Loaded Successfully!");
+/* Forms */
+
+.form-section{
+
+padding:60px 10%;
+
+display:flex;
+
+flex-direction:column;
+
+align-items:center;
+
+}
+
+.form-section h1{
+
+color:#2E7D32;
+
+margin-bottom:30px;
+
+}
+
+form{
+
+background:white;
+
+padding:35px;
+
+width:100%;
+
+max-width:650px;
+
+border-radius:12px;
+
+box-shadow:0 5px 15px rgba(0,0,0,.1);
+
+}
+
+input,
+select,
+textarea{
+
+width:100%;
+
+padding:15px;
+
+margin-bottom:20px;
+
+border:1px solid #ccc;
+
+border-radius:8px;
+
+font-size:16px;
+
+}
+
+textarea{
+
+resize:none;
+
+}
+
+form button{
+
+width:100%;
+
+}
